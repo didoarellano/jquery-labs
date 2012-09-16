43dcb03cd403b84bbafd29e102206339ff4c6128
Dido Arellano <dido@semicolonstudios.com>
2012-09-16 17:58:10 +0800

    Hook into the window's hashchange event

--- app/js/labs.js
+++ app/js/labs.js
@@ -2,13 +2,38 @@ define(['jquery'], function($) {
 
     "use strict";
 
+    var body = document.body;
+    var $win = $(window);
     var labs = {};
 
     labs.init = function() {
-        console.log('labs: ', this);
-        console.log('jQuery: ', $);
+        if (body.className === 'index') {
+            appendSavePoints();
+        }
+
+        loadExercises();
+        $win.on('hashchange', loadExercises);
     };
 
     return labs;
+
+    function appendSavePoints() {
+        // TODO: Get savePoints from localStorage once we're saving data
+        $('.categories a').each(function(i, el) {
+            el.href = el.href + '/1';
+        });
+    }
+
+    function loadExercises() {
+        // #/selecting/1
+        var hash = window.location.hash.split('/');
+        var category = hash[1];
+        var savePoint = parseInt(hash[2], 10);
+
+        if (!category) { return; }
+
+        console.log(category, savePoint);
+
+    }
 
 });
