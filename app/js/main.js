7dc28d8db00b80d518e5fd55e7a06538544309d5
Dido Arellano <dido@semicolonstudios.com>
2012-09-16 16:59:20 +0800

    Load CDN jquery w/ fallback; local if developing

--- app/js/main.js
+++ app/js/main.js
@@ -1,10 +1,27 @@
-require.config({
-    paths: {
-        jquery: 'vendor/jquery-1.8.1'
-    }
-});
+(function(window, require) {
 
-require(['labs'], function(labs) {
     "use strict";
-    labs.init();
-});
+
+    var config = {
+        paths: {
+            jquery: [
+                '//ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min',
+                'vendor/jquery-1.8.1.min'
+            ]
+        }
+    };
+
+    var location = window.location;
+
+    if (location.protocol === 'file:' || location.hostname === 'localhost') {
+        config.paths = {
+            jquery: 'vendor/jquery-1.8.1'
+        };
+    }
+
+    require.config(config);
+    require(['labs'], function(labs) {
+        labs.init();
+    });
+
+})(window, require);
