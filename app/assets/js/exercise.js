95b821884a56a8bc09a9a6393163957a50605306
Dido Arellano <dido@semicolonstudios.com>
2012-09-17 19:58:40 +0800

    Add more properties to Exercise constructor

--- app/assets/js/exercise.js
+++ app/assets/js/exercise.js
@@ -1,9 +1,17 @@
 define(function() {
 
+    "use strict";
+
     return Exercise;
 
     function Exercise(config) {
-        this.iframeSrc = config.iframeSrc;
+        this.iframeName = config.iframeName;
+        this.iframePath = config.iframePath || 'assets/exercises';
+        this.iframeSrc = this.iframePath + '/' + config.iframeName + '.html';
+        this.iframeHTML = '';
+        this.cmd = '';
+        this.instructions = config.instructions;
+        this.selector = config.selector;
     }
 
 });
--- test/spec/test-exercise.js
+++ test/spec/test-exercise.js
@@ -5,15 +5,56 @@ define(['/app/assets/js/exercise.js'], function(Exercise) {
         // Set up the Exercise instance //-v-
         var exercise, exerciseConfig;
         exerciseConfig = {
-            iframeSrc: 'next.html'
+            instructions: 'This is what you should do.',
+            selector: '#context',
+            iframePath: 'fake/path',
+            iframeName: 'next'
         };
         beforeEach(function() {
             exercise = new Exercise(exerciseConfig);
         });
         //-^-
 
-        it('should allow an iframeSrc property to be set', function() {
-            exercise.iframeSrc.should.equal('next.html');
+        it('should expose config properties as instance properties', function() {
+
+            exercise.should.have.property('instructions')
+                .and.equal('This is what you should do.');
+
+            exercise.should.have.property('selector')
+                .and.equal('#context');
+
+            exercise.should.have.property('iframePath')
+                .and.equal('fake/path');
+
+            exercise.should.have.property('iframeName')
+                .and.equal('next');
+
+        });
+
+        it('should set iframeHTML & cmd as empty string properties', function() {
+            exercise.iframeHTML.should.equal('');
+            exercise.cmd.should.equal('');
+        });
+
+        it('should create iframeSrc property', function() {
+            exercise.iframeSrc.should.equal('fake/path/next.html');
+        });
+
+
+        describe('Default values', function() {
+
+            var defVal;
+
+            beforeEach(function() {
+                defVal = new Exercise({
+                    iframeName: 'prev'
+                })
+            });
+
+            it('should default to an iframePath property of assets/exercises', function() {
+                defVal.iframePath.should.equal('assets/exercises');
+                defVal.iframeSrc.should.equal('assets/exercises/prev.html');
+            });
         });
 
     });
