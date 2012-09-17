7b0daa48a212c5a909b48c2b5867827bbf03f4a0
Dido Arellano <dido@semicolonstudios.com>
2012-09-17 18:21:22 +0800

    Cache bust all modules on each refresh on testing

--- test/specrunner.js
+++ test/specrunner.js
@@ -2,6 +2,13 @@
 
     "use strict";
 
+    // Append a cache-busting querystring because Canary's caching is very
+    // aggressive even with "Disable cache" checked. Could it also be python's
+    // SimpleHTTPServer?
+    require.config({
+        urlArgs: "v="+(new Date()).getTime()
+    });
+
     var specs = [
         'spec/test-exercise'
     ];
