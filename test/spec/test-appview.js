818bd7d6345d9160b0e2958a7aface52de3d0d7e
Dido Arellano <dido@semicolonstudios.com>
2012-09-29 15:26:25 +0800

    Add more appview selectors to test

--- test/spec/test-appview.js
+++ test/spec/test-appview.js
@@ -9,10 +9,12 @@ define(['appview'], function(AppView) {
         var appview = new AppView();
 
         it('should cache selectors on instantiation', function() {
+            expect(appview.$window).to.exist;
             expect(appview.body).to.exist;
             expect(appview.$container).to.exist;
             expect(appview.$index).to.exist;
             expect(appview.$exercise).to.exist;
+            expect(appview.$views).to.exist;
             expect(appview.$startScreens).to.exist;
             expect(appview.$sidebar).to.exist;
         });
