4575d6150dd2f8b2274c0e7d7046a133d4255bc5
Dido Arellano <dido@semicolonstudios.com>
2013-09-08 16:47:23 +0800

    Add categories fixture data

---
+++ app/assets/data/categories.js
@@ -0,0 +1,18 @@
+var jQLabsData = jQLabsData || {};
+jQLabsData.categories = [
+    {
+        "id": "selecting",
+        "title": "Selecting Elements",
+        "description": "<p>Sed tincidunt diam id sem commodo auctor. Mauris molestie venenatis enim et gravida. Vestibulum malesuada arcu eget elit facilisis viverra. Ut facilisis condimentum blandit. Suspendisse potenti. Aliquam erat volutpat. Vivamus congue dictum nunc, in mattis libero molestie id. Pellentesque sit amet dolor hendrerit, condimentum odio eu, pellentesque turpis.</p> <p>Quisque venenatis euismod egestas. Fusce auctor tortor eu tempus consequat. Ut dapibus imperdiet risus, quis laoreet nunc rhoncus sed. Aliquam volutpat est turpis, quis ultrices arcu vestibulum vitae. Aenean blandit euismod nisl, quis blandit mauris suscipit nec. Curabitur a leo eget dui rhoncus condimentum. Nunc aliquet posuere neque a imperdiet. Nullam lorem orci, faucibus nec elit in, scelerisque fringilla erat. Fusce accumsan velit nec blandit volutpat. Sed non luctus nibh.</p>"
+    },
+    {
+        "id": "traversing",
+        "title": "Traversing to Other Elements",
+        "description": "<p>Sed tincidunt diam id sem commodo auctor. Mauris molestie venenatis enim et gravida. Vestibulum malesuada arcu eget elit facilisis viverra. Ut facilisis condimentum blandit. Suspendisse potenti. Aliquam erat volutpat. Vivamus congue dictum nunc, in mattis libero molestie id. Pellentesque sit amet dolor hendrerit, condimentum odio eu, pellentesque turpis.</p> <p>Quisque venenatis euismod egestas. Fusce auctor tortor eu tempus consequat. Ut dapibus imperdiet risus, quis laoreet nunc rhoncus sed. Aliquam volutpat est turpis, quis ultrices arcu vestibulum vitae. Aenean blandit euismod nisl, quis blandit mauris suscipit nec. Curabitur a leo eget dui rhoncus condimentum. Nunc aliquet posuere neque a imperdiet. Nullam lorem orci, faucibus nec elit in, scelerisque fringilla erat. Fusce accumsan velit nec blandit volutpat. Sed non luctus nibh.</p>"
+    },
+    {
+        "id": "filtering",
+        "title": "Filtering the Collection",
+        "description": "<p>Sed tincidunt diam id sem commodo auctor. Mauris molestie venenatis enim et gravida. Vestibulum malesuada arcu eget elit facilisis viverra. Ut facilisis condimentum blandit. Suspendisse potenti. Aliquam erat volutpat. Vivamus congue dictum nunc, in mattis libero molestie id. Pellentesque sit amet dolor hendrerit, condimentum odio eu, pellentesque turpis.</p> <p>Quisque venenatis euismod egestas. Fusce auctor tortor eu tempus consequat. Ut dapibus imperdiet risus, quis laoreet nunc rhoncus sed. Aliquam volutpat est turpis, quis ultrices arcu vestibulum vitae. Aenean blandit euismod nisl, quis blandit mauris suscipit nec. Curabitur a leo eget dui rhoncus condimentum. Nunc aliquet posuere neque a imperdiet. Nullam lorem orci, faucibus nec elit in, scelerisque fringilla erat. Fusce accumsan velit nec blandit volutpat. Sed non luctus nibh.</p>"
+    }
+];
