1a557d7cb12092fec346355e41b3c0fbb7858e83
Dido Arellano <dido@semicolonstudios.com>
2013-09-11 16:23:23 +0800

    Add Categories & Category routes

--- app/assets/js/routes.js
+++ app/assets/js/routes.js
@@ -1,5 +1,23 @@
+App.Router.map(function() {
+    this.resource('categories', { path: ':category_id' }, function() {
+        this.route('category', { path: '' });
+    });
+});
+
 App.IndexRoute = Ember.Route.extend({
     model: function() {
         return App.Category.find();
+    }
+});
+
+App.CategoriesRoute = Ember.Route.extend({
+    model: function(params) {
+        return App.Category.find(parmas.category_id);
+    }
+});
+
+App.CategoriesCategoryRoute = Ember.Route.extend({
+    model: function() {
+        return this.modelFor('categories');
     }
 });
