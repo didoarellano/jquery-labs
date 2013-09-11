2427f09434b3ad13e2ef723c0e3103832705aeb4
Dido Arellano <dido@semicolonstudios.com>
2013-09-12 01:34:43 +0800

    Add iframe SandboxView

---
+++ app/assets/js/view-sandbox.js
@@ -0,0 +1,24 @@
+App.SandboxView = Ember.View.extend({
+    tagName: 'iframe',
+    attributeBindings: ['src'],
+    src: 'assets/sandbox/sandbox.html',
+    window: null,
+    body: null,
+
+    didInsertElement: function() {
+        var iframe = this.$()[0];
+        var html = this.get('content.htmlStart');
+
+        iframe.onload = function() {
+            var body = iframe.contentDocument.body;
+            body.innerHTML = html;
+            this.set('window', iframe.contentWindow);
+            this.set('body', body);
+            iframe.onload = null;
+        }.bind(this);
+    },
+
+    changeExercise: function() {
+        this.rerender();
+    }.observes('exercise')
+});
