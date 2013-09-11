3598b313e56b453038c4a1dcbb29926fe3eef54c
Dido Arellano <dido@semicolonstudios.com>
2013-09-11 17:18:09 +0800

    Add Exercises/index routes

--- app/assets/js/routes.js
+++ app/assets/js/routes.js
@@ -1,6 +1,9 @@
 App.Router.map(function() {
     this.resource('categories', { path: ':category_id' }, function() {
         this.route('category', { path: '' });
+        this.resource('exercises', function() {
+            this.route('index', { path: '' })
+        });
     });
 });
 
@@ -21,3 +24,15 @@ App.CategoriesCategoryRoute = Ember.Route.extend({
         return this.modelFor('categories');
     }
 });
+
+App.ExercisesRoute = Ember.Route.extend({
+    model: function() {
+        return this.modelFor('categories').getExercises();
+    }
+})
+
+App.ExercisesIndexRoute = Ember.Route.extend({
+    model: function() {
+        return this.modelFor('exercises');
+    },
+})
