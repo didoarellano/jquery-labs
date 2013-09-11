af22fcca59adfcaedcdbac57ba82b1d17c111c0a
Dido Arellano <dido@semicolonstudios.com>
2013-09-12 02:27:25 +0800

    Add Prism highlighter as Ember component

---
+++ app/assets/js/component-prism.js
@@ -0,0 +1,16 @@
+App.PrismHighlighterComponent = Ember.Component.extend({
+    didInsertElement: function() {
+        var code = this.get('code');
+        var language = this.get('language');
+        if (language === 'markup') {
+            // Prism's markup grammar expects only "<" chars to be escaped.
+            code = code.replace(/</g, '&lt;');
+        }
+        var highlighted = Prism.highlight(code, Prism.languages[language]);
+        this.set('highlighted', highlighted);
+    },
+
+    changeExercise: function() {
+        this.rerender();
+    }.observes('exercise')
+});
