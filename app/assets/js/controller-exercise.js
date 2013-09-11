6d2e8b2d1764f63adc019403b238b332839b59cc
Dido Arellano <dido@semicolonstudios.com>
2013-09-12 00:10:38 +0800

    Make prev & next exercise buttons work

--- app/assets/js/controller-exercise.js
+++ app/assets/js/controller-exercise.js
@@ -1,7 +1,19 @@
 App.ExerciseController = Ember.ObjectController.extend({
     needs: ['categories', 'exercises'],
 
+    getExercise: function(id) {
+        return this.get('controllers.categories.model').findExerciseById(id);
+    },
+
     allExercises: function() {
         return this.get('controllers.exercises.exercises');
-    }.property('controllers.exercises.exercises')
+    }.property('controllers.exercises.exercises'),
+
+    prevExercise: function() {
+        return this.getExercise(this.get('id') - 1);
+    }.property('id'),
+
+    nextExercise: function() {
+        return this.getExercise(this.get('id') + 1);
+    }.property('id')
 });
